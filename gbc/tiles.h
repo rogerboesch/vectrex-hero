dcbf3f9d61c8ff7ab817dba8d885533aff00a52a
Roger Boesch <mail@rogerboesch.com>
2026-04-12 13:28:52 +0200

    Extend tile ranges: destroyable 64-79, decorative 80-95

--- gbc/tiles.h
+++ gbc/tiles.h
@@ -13,7 +13,7 @@
 #define TILE_EMPTY       0
 #define TILE_WALL        1     // main wall tile (for title screen decorations)
 #define TILE_DWALL_FIRST 64    // destroyable wall range start
-#define TILE_DWALL_LAST  66    // destroyable wall range end
+#define TILE_DWALL_LAST  79    // destroyable wall range end
 #define TILE_LAVA0       19    // lava frame 0 (collision check)
 #define TILE_LAVA1       20    // lava frame 1
 #define TILE_DECOR_FIRST 80    // decorative tiles (no collision)
